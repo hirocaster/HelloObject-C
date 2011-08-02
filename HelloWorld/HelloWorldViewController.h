770e5118b71a062fee225439779ef7e277e1e2ac
hirocaster <hirocast@gmail.com>
2011-08-02 20:06:38 +0900

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by Hiroki Ohtsuka on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController {
+    
+}
+
+@end
---
+++ HelloWorldTests/HelloWorldTests.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.h
+//  HelloWorldTests
+//
+//  Created by Hiroki Ohtsuka on 8/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface HelloWorldTests : SenTestCase {
+@private
+    
+}
+
+@end
